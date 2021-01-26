e624219a7a1eac32c8962c4092f5175ccc39b24d
John Kenedy <john.kenedy0410@gmail.com>
2021-01-26 16:32:58 -0300

    RecyclerView Videos

--- app/src/main/java/com/canytech/youtubeapp/MainActivity.kt
+++ app/src/main/java/com/canytech/youtubeapp/MainActivity.kt
@@ -1,16 +1,66 @@
 package com.canytech.youtubeapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.OkHttpClient as OkHttpClient1
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var videoAdapter: VideoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val videos: MutableList<Video> = mutableListOf<Video>()
+        videoAdapter = VideoAdapter(videos) { video: Video ->
+            println(video)
+        }
+
         rv_main.layoutManager = LinearLayoutManager(this)
-        rv_main.adapter = VideoAdapter()
+        rv_main.adapter = videoAdapter
+
+        CoroutineScope(Dispatchers.IO).launch {
+           val res =  async { getVideo() }
+            val listVideo = res.await()
+            withContext(Dispatchers.Main) {
+                listVideo?.let {
+                    videos.clear()
+                    videos.addAll(listVideo.data)
+                    videoAdapter.notifyDataSetChanged()
+                    motion_container.removeView(progress_recycler)
+
+                }
+            }
+        }
+        getVideo()
+    }
+
+    private fun getVideo(): ListVideo? {
+        val client: OkHttpClient1 = OkHttpClient1.Builder()
+            .build()
+
+        val request:Request = Request.Builder()
+            .get()
+            .url("https://tiagoaguiar.co/api/youtube-videos")
+            .build()
+
+        return try {
+            val response:Response = client.newCall(request).execute()
+
+            if (response.isSuccessful) {
+GsonBuilder().create()
+    .fromJson(response.body()?.string(), ListVideo::class.java)
+            } else {
+                null
+            }
+        } catch (e: Exception) {
+            null
+        }
     }
 }
